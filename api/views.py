931b98f12379007b277117a48c436bd1fb512280
Santiago <sanjoibarra007@gmail.com>
2022-02-03 15:19:34 -0300

    templates terminadas

--- api/views.py
+++ api/views.py
@@ -4,7 +4,6 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect
-from django.contrib import messages
 
 from api.models import Product
 
